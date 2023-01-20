7095c86bc2c77b851b8f23d4c83c02c9aa732232
Armando Murillo <totovip1464@gmail.com>
2023-01-20 14:45:51 -0600

    PostLists.jsx: Arreglo los posts por fecha (de mas actual a menos actual)

--- components/posts/PostsLists.jsx
+++ components/posts/PostsLists.jsx
@@ -2,11 +2,13 @@ import { PostCard } from "./"
 
 
 export const PostsLists = ({ posts }) => {
+
+    const postsSort = posts.sort((a,b) => new Date(b.date).getTime() - new Date(a.date).getTime())
     
     return(
         <>
             {
-                posts.map(item => (
+                postsSort.map(item => (
                     < PostCard title={item.title} date={ item.date } slug={item.slug} img={item.imgTitle} key={item.slug} desc={item.description} />
                 ))
             }
