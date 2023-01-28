43ca75543793ceb00e7b83026e2aef86309c780c
Armando Murillo <totovip1464@gmail.com>
2023-01-27 20:03:29 -0600

    Cito la frase con comillas

--- components/home/about/About.jsx
+++ components/home/about/About.jsx
@@ -17,7 +17,7 @@ export const About = () => {
                     Estudiante de Ing. Informática (2020 - 2023), Tengo más de 2 años especializandome en el área de 
                     desarrollo web. Dedico gran parte de mi tiempo a crecer como desarrollador, generando experiencia
                     desde la construcción de distintos proyectos. Y por otra parte, compartiendo todo recurso que le pueda
-                    ser de ayuda a cualquier otro desarrollador. <em>Cuando uno enseña, dos aprenden.</em> - <strong>Robert Heinlein</strong>
+                    ser de ayuda a cualquier otro desarrollador. <em>{`"Cuando uno enseña, dos aprenden."`}</em> - <strong>Robert Heinlein</strong>
                 </p>
             </div>
         </>
