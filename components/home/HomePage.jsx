b46730873929d08b04eeb86aeab0e0e24538bcb5
Armando Murillo <totovip1464@gmail.com>
2023-01-27 18:49:01 -0600

    HomePage.jsx: Agrego nueva seccion (sobre mi)

--- components/home/HomePage.jsx
+++ components/home/HomePage.jsx
@@ -1,10 +1,10 @@
-import { Podcast, PostInstagram, Youtube } from "../frames"
 import { PostsLists } from "../posts"
 
 import { usePagination } from "@/hooks"
 
-import { FaArrowRight, FaArrowLeft } from "react-icons/fa"
 import { ButtonsPagination } from "./buttons/ButtonsPagination"
+import { About } from "./about/About"
+import { Social } from "./social/Social"
 
 export const HomePage = ( {posts} ) => {
 
@@ -25,22 +25,16 @@ export const HomePage = ( {posts} ) => {
                     postsLength={posts.length} 
                 />
 
-                </div>
-                <div className="container-posts-social">
-                    <h1>Último video en Youtube</h1>
-                    <div className="posts-youtube">
-                        < Youtube 
-                            urlVideo={'https://www.youtube.com/watch?v=bA6T8KOgAFg'}
-                            urlImage={'https://res.cloudinary.com/dyuj1zglt/image/upload/v1674766529/miniaturas/wg8lwf9mospmukj5fgdu.png'} 
-                            description='Closures en JAVASCRIPT: Qué son, y cómo se usan. 👨‍💻'
-                        />
-                    </div>
-                    <h1>Podcast recomendados</h1>
-                    <div className="posts-podcast">
-                        < Podcast episode={'3oosd3Y7oM3DAsXusY7rai'} />
-                        < Podcast episode={'5fd78Wp41IhaoKWVKBoS3C'} />
-                    </div>
-                </div>
+                <section className="container-info-autor">
+                    <About />
+                </section>
+
+            </div>
+
+            <section className="container-posts-social">
+                <Social />
+            </section>
+               
         </section>
     )
 }
