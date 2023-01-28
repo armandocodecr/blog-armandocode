8cdd2fde2292f1a78fb1cb79dccee9a01ec88897
Armando Murillo <totovip1464@gmail.com>
2023-01-27 19:07:54 -0600

    About.jsx: Elimino comillas en la etiqueta "p"

--- components/home/about/About.jsx
+++ components/home/about/About.jsx
@@ -7,9 +7,7 @@ export const About = () => {
             <h1>Sobre mi</h1>
 
             <div className="info-autor">
-                <div 
-                    className="title-info-autor"
-                >
+                <div className="title-info-autor">
                     <picture>
                         <img src="/img/foto.png" alt="Aqui muestro una imagen de mi persona" />
                     </picture>
@@ -19,7 +17,7 @@ export const About = () => {
                     Estudiante de Ing. Informática (2020 - 2023), Tengo más de 2 años especializandome en el área de 
                     desarrollo web. Dedico gran parte de mi tiempo a crecer como desarrollador, generando experiencia
                     desde la construcción de distintos proyectos. Y por otra parte, compartiendo todo recurso que le pueda
-                    ser de ayuda a cualquier otro desarrollador. <em>"Cuando uno enseña, dos aprenden.</em> - <strong>Robert Heinlein</strong>
+                    ser de ayuda a cualquier otro desarrollador. <em>Cuando uno enseña, dos aprenden.</em> - <strong>Robert Heinlein</strong>
                 </p>
             </div>
         </>
