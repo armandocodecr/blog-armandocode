bb09e5248389ca7e53d6e7ca8ee7e8d129d10650
ArmandoCode <totovip1464@gmail.com>
2023-08-31 17:46:25 -0600

    add link to my portfolio in my footer

--- components/ui/Footer.jsx
+++ components/ui/Footer.jsx
@@ -3,7 +3,13 @@
 export const Footer = () => {
     return (
         <>
-            <h3>Web developer <span>Armando Cortés Murillo</span>. 2023</h3>
+            <h3>
+                Web developer 
+                <a href="https://armando-murillo.vercel.app/" target="_blank" rel="noreferrer">
+                    Armando Cortés Murillo
+                </a>
+                . 2023
+            </h3>
         </>
     )
 }
